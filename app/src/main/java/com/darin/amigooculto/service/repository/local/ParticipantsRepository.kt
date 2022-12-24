4d580e5ac40994c47acf78fc81ef957466697877
darinformax@gmail.com <darinformax@gmail.com>
2022-12-24 16:10:14 -0300

    repository layers created with the secretsantadb and the participants table

---
+++ app/src/main/java/com/darin/amigooculto/service/repository/local/ParticipantsRepository.kt
@@ -0,0 +1,29 @@
+package com.darin.amigooculto.service.repository.local
+
+import android.content.Context
+import com.darin.amigooculto.service.repository.local.database.SecretSantaDatabase
+import com.darin.amigooculto.service.repository.local.databasemodels.ParticipantModel
+
+class ParticipantsRepository private constructor(context: Context) {
+
+    private val database = SecretSantaDatabase.getDatabase(context).participantDAO()
+
+    fun insert(participant: ParticipantModel): Boolean {
+        return database.insert(participant) > 0
+    }
+
+    fun update(participant: ParticipantModel): Boolean {
+        return database.update(participant) > 0
+    }
+
+    fun delete(id: Int) {
+        val participant = ParticipantModel()
+        participant.id = id
+        database.delete(participant)
+    }
+
+    fun getAll(): List<ParticipantModel> {
+        return database.getAll()
+    }
+
+}
